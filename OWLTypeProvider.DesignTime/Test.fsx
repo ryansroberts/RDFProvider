4547e7bbd545690976b12303958e1f8ee558c475
Ryan Roberts <ryansroberts@gmail.com>
2014-07-02 16:07:54 +0100

    Test gubbins to dump generated types

---
+++ OWLTypeProvider.DesignTime/Test.fsx
@@ -0,0 +1,40 @@
+﻿#if INTERACTIVE
+#load "../GenerateSetupScript.fsx"
+
+GenerateSetupScript.generateSetupScript __SOURCE_DIRECTORY__ "OWLTypeProvider.DesignTime"
+#load "__setup__OWLTypeProvider.DesignTime__.fsx"
+#else
+module internal Test
+#endif
+
+
+open System
+open System.IO
+open System.Net
+open LinkedData
+
+let (++) a b = Path.Combine(a, b)
+let resolutionFolder = ""
+let outputFolder = __SOURCE_DIRECTORY__ ++ ".." ++ "OWLTypeProvider.Test" ++ "expected"
+let assemblyName = "OWLTypeProvider.DesignTime.dll"
+
+type Platform = Net40
+open TypeProviderInstantiation
+
+let dump signatureOnly ignoreOutput platform saveToFileSystem (inst:TypeProviderInstantiation) =
+    let runtimeAssembly = 
+        match platform with
+        | Net40 -> __SOURCE_DIRECTORY__ ++ ".." ++ "bin" ++ assemblyName
+    inst.Dump resolutionFolder (if saveToFileSystem then outputFolder else "") runtimeAssembly signatureOnly ignoreOutput
+    |> Console.WriteLine
+
+let dumpAll inst = 
+    dump false false Net40 true inst
+
+let args = { 
+  Path = __SOURCE_DIRECTORY__ ++ "wine.ttl"
+  BaseUri = "http://www.w3.org/2002/07/owl#Thing"
+  NSMap = ""
+}
+
+dumpAll (TypeProviderInstantiation.FileProvider (args))
---
+++ OWLTypeProvider.DesignTime/TypeProviderInstantiation.fs
@@ -0,0 +1,51 @@
+﻿module TypeProviderInstantiation
+
+open System
+open System.IO
+open ProviderImplementation
+open ProviderImplementation.ProvidedTypes
+open LinkedData
+
+type FileProviderArgs = 
+    { Path : string
+      BaseUri : string
+      NSMap : string }
+
+type TypeProviderInstantiation = 
+    | FileProvider of FileProviderArgs
+    
+    member x.GenerateType resolutionFolder runtimeAssembly = 
+        let f, args = 
+            match x with
+            | FileProvider x -> 
+                (fun cfg -> new FileProvider(cfg) :> TypeProviderForNamespaces), 
+                [| box x.Path
+                   box x.BaseUri
+                   box x.NSMap |]
+        Debug.generate resolutionFolder runtimeAssembly Platform.Full f args
+    
+    override x.ToString() = 
+        match x with
+        | FileProvider x -> [ "FileProvider"; x.Path; x.BaseUri; x.NSMap ]
+        |> String.concat ","
+    
+    member x.ExpectedPath outputFolder = 
+        Path.Combine
+            (outputFolder, 
+             
+             (x.ToString().Replace(">", "&gt;").Replace("<", "&lt;").Replace(".", "_").Replace("://", "_")
+               .Replace("\\", "_").Replace("/", "_").Replace(",", "_").Replace("#","_") + ".expected"))
+    member x.Dump resolutionFolder outputFolder runtimeAssembly signatureOnly ignoreOutput = 
+        let replace (oldValue : string) (newValue : string) (str : string) = str.Replace(oldValue, newValue)
+        
+        let output = 
+            x.GenerateType resolutionFolder runtimeAssembly
+            |> match x with
+               | _ -> Debug.prettyPrint signatureOnly ignoreOutput 10 100
+            |> replace "FSharp.Data.Runtime." "FDR."
+            |> if String.IsNullOrEmpty resolutionFolder then id
+               else replace resolutionFolder "<RESOLUTION_FOLDER>"
+        if outputFolder <> "" then File.WriteAllText(x.ExpectedPath outputFolder, output)
+        output
+
+open System.Runtime.CompilerServices
